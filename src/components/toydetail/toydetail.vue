0c5021feb6cb44ef74d1fd92d7143a854e29baa3
Andy <zgyandy@126.com>
2017-10-30 17:19:22 +0800

    first commit

--- src/components/toydetail/toydetail.vue
+++ src/components/toydetail/toydetail.vue
@@ -1,24 +1,42 @@
 <template>
     <div class="toydetail">
-        <ul class="navbar">
-            <li>玩具1</li>
-            <li>玩具2</li>
-            <li>玩具3</li>
-        </ul>
-        <ul>
-            <li>
-                玩具娃娃
-            </li>
-            <li>
-                玩具娃娃
-            </li>
-            <li>
-                玩具娃娃
-            </li>
-            <li>
-                玩具娃娃
-            </li>
-        </ul>
+        <mt-navbar v-model="selected" value=1>
+            <mt-tab-item id="1">选项一</mt-tab-item>
+            <mt-tab-item id="2">选项二</mt-tab-item>
+            <mt-tab-item id="3">选项三</mt-tab-item>
+        </mt-navbar>
+        <mt-tab-container v-model="selected">
+            <mt-tab-container-item id="1">
+                <ul>
+                    <li>
+                        <img src="../../assets/images/demo1.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../../assets/images/demo1.png" alt="">
+                    </li>
+                </ul>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="2">
+                <ul>
+                    <li>
+                        <img src="../../assets/images/demo2.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../../assets/images/demo2.png" alt="">
+                    </li>
+                </ul>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="3">
+                <ul>
+                    <li>
+                        <img src="../../assets/images/demo1.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../../assets/images/demo1.png" alt="">
+                    </li>
+                </ul>
+            </mt-tab-container-item>
+        </mt-tab-container>
     </div>
 </template>
 
@@ -26,25 +44,33 @@
 export default {
   props: {
     toy_id: {
-      default: Number
+            default: Number
+        }
+    },
+    data(){
+        return {
+            num: 1,
+            selected: "1",
+        }
     }
-  }
+
 };
 </script>
 
 <style lang="scss" scoped>
 .toydetail {
-  width: 6.9rem;
-  margin: 0 0.3rem;
-  background-color: #f00;
-  .navbar {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    li {
-      height: 0.8rem;
-      line-height: 0.8rem;
+    width: 6.9rem;
+    margin: 0 0.3rem;
+    .mint-navbar .mint-tab-item {
+        font-size: .3rem;
+        .is-selected {
+            color: #0ff;
+        }
     }
-  }
+    .mint-tab-container-item {
+        img {
+            width: 100%;
+        }        
+    }
 }
 </style>
